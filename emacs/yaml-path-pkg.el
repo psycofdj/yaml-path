cf074f50c9f255f3c7007b0654fb6c4a400acdc8
Xavier MARCELET <xavier@marcelet.com>
2018-12-14 21:33:54 +0100

    bump emacs plugin 0.4

--- emacs/yaml-path-pkg.el
+++ emacs/yaml-path-pkg.el
@@ -1,4 +1,4 @@
-(define-package "yaml-path" "0.3" "Get path under cursor in a yaml flie"
+(define-package "yaml-path" "0.4" "Get path under cursor in a yaml flie"
   '()
   :url "https://github.com/psycofdj/yaml-path"
   )
